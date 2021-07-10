7a40a820c842bfda38282d526471dc316cc64c25
varan5 <varan2.bhalla@gmail.com>
2021-07-10 20:09:27 +0530

    React router dom installed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <div className="navbar-container container"></div>
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            LAVISH
+          </Link>
+        </div>
       </div>
     </>
   )
